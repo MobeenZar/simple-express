754fa6d3399ff5fdcabce1eb38ff4a2e77925d72
Mobeen Zar <mobeen_zar@hotmail.com>
2021-06-11 20:33:40 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app.get("/users/:username", (req, res, next) => {
+  let username = req.params.username;
+  let pid = uuidv4();
+
+  console.log(new Date().toLocaleString() + `: ${pid} - User ${username} requested for resource`);
+  //sleep(Math.random() * 10000).then(() => {
+  console.log(new Date().toLocaleString() + `: ${pid} - Finished processing request for ${username}`);
+  //})
+
+  res.status(200).send("Message from user: " + username); 
+});
+
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Hello world!");
+});
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("Wellcome!");
+});
+
+
